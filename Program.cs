2b5feeae1a34fbafb2ad1aa0c08cdd0ab084bf04
femaffi13 <fedemaffi13@gmail.com>
2021-05-07 17:56:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace myfirstapp
+{
+    class Program
+    {
+        static void Main(){
+            Random num = new Random();
+
+            int numAleatorio = num.Next(0,100);
+            
+            Console.WriteLine($"El valor es: {numAleatorio}");
+        }            
+    }
+}
+
+
+
