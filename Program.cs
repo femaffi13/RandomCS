ab88e5e9758f3daeeaad66e2509e0c22c6af084a
femaffi13 <fedemaffi13@gmail.com>
2021-05-07 18:05:14 -0300

    Num Aleatorio

--- Program.cs
+++ Program.cs
@@ -6,10 +6,22 @@ namespace myfirstapp
     {
         static void Main(){
             Random num = new Random();
-
             int numAleatorio = num.Next(0,100);
-            
-            Console.WriteLine($"El valor es: {numAleatorio}");
+
+            int miNum = 101;   
+            int intentos = 0;
+
+            Console.WriteLine("Introducir un valor entre 0 y 100");
+
+            while(numAleatorio != miNum){
+                miNum = int.Parse(Console.ReadLine());
+                if(miNum > numAleatorio) => Console.WriteLine("El número es más bajo");
+                if(miNum < numAleatorio) => Console.WriteLine("El número es más alto");
+
+                intentos++;
+            }
+
+            Console.WriteLine($"Correcto! Acertaste en ${intentos} intentos");
         }            
     }
 }
